3fa603ec9fddb41673b0f563538fafe19558c399
Khanh Luc <itsmekhanh@gmail.com>
2015-08-01 23:45:44 -0700

    navbar is complete!

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
     var app = app || {};
 
+    app.showMenu = true;
+
     $("#navigation-icon").on("click", function(){
-       $("#navigation-menu").slideToggle();
+        $("#navigation-menu").slideToggle({
+            complete: function(){
+                app.showMenu = !app.showMenu;
+                if(app.showMenu)
+                    $("#navigation-menu").removeAttr("style");
+            }
+        });
     });
 });
