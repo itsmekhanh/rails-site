2f9f611d3f8c02ff8ad163c698c5e55008cfbfce
Khanh Luc <itsmekhanh@gmail.com>
2015-08-15 20:41:41 -0700

    still working on video post

--- app/assets/javascripts/components/main_post.js.jsx
+++ app/assets/javascripts/components/main_post.js.jsx
@@ -3,19 +3,33 @@ var MainPost = React.createClass({
         var img = "";
         var divStyle = {};
         var body = "";
+        var textPost = null;
 
-        post = this.props.post
-        var $body = $("<div>"+post.body+"</div>");
+        post = this.props.post;
 
         switch(post.type){
             case "text":
+                var $body = $("<div>"+post.body+"</div>");
                 var imgTag = $body.find("img").first();
+
                 $body.find('img').first().remove();
                 img = imgTag[0].src;
                 body = $body.html();
+                divStyle['backgroundImage'] = 'url('+img+')';
+                textPost = (
+                    <div id="main-image" style={divStyle}>
+                        <div className="overlay">
+                            <div className="text-center vcenter">
+                                <h2>{post.title}</h2>
+                                <h3>{post.date}</h3>
+                            </div>
+                        </div>
+                    </div>
+                );
                 break;
             case "video":
-                body = post.player[2];
+                body = post.body;
+                img = post.thumbnail_url;
                 break;
             case "photo":
                 break;
@@ -25,18 +39,9 @@ var MainPost = React.createClass({
                 break;
         }
 
-        divStyle['backgroundImage'] = 'url('+img+')';
-
         return (
             <div id="main-post">
-                <div id="main-image" style={divStyle}>
-                    <div className="overlay">
-                        <div className="text-center vcenter">
-                            <h2>{post.title}</h2>
-                            <h3>{post.date}</h3>
-                        </div>
-                    </div>
-                </div>
+                { textPost }
                 <div id="post" dangerouslySetInnerHTML={{__html: body}} />
             </div>
         );
