0177a9e4c7464a8295cd3d43cabc79de08f80ea2
Khanh Luc <itsmekhanh@gmail.com>
2015-08-26 14:11:55 -0700

    still playing around with some asset settings

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,152 @@
+$(document).ready(function(){
+    // charts
+    new Chartist.Pie('.ct-chart-php', {
+        series: [95,5]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-ruby', {
+        series: [60,40]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-python', {
+        series: [50,50]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-java', {
+        series: [50,50]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-javascript', {
+        series: [95,5]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-html', {
+        series: [90,10]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-css', {
+        series: [85,20]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-react', {
+        series: [70,30]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-mysql', {
+        series: [90,10]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-mariadb', {
+        series: [90,10]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-mongodb', {
+        series: [50,50]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-photography', {
+        series: [95,5]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-drawing', {
+        series: [90,10]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-painting', {
+        series: [90,10]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+
+    new Chartist.Pie('.ct-chart-dancing', {
+        series: [10,90]
+    }, {
+        donut: true,
+        donutWidth: 50,
+        startAngle: 0,
+        total: 100,
+        showLabel: false
+    });
+});
